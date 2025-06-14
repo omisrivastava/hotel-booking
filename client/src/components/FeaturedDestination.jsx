e6a098f188174f58d2cba753dc4d7924a235a365
Sudhanshu Ranjan <sudhanshuranjan2k18@gmail.com>
2025-06-14 13:10:09 +0000

    Fixed multiple UI issues

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -1,16 +1,14 @@
-import React from "react";
 import { roomsDummyData } from "../assets/assets.js";
 import Hotelcard from "../components/Hotelcard.jsx";
 
 const FeaturedDestination = () => {
   return (
     <div>
-      <div>
+      <div className="mx-10 flex flex-wrap gap-10 mt-10">
         {roomsDummyData.slice(0, 4).map((room, index) => (
           <Hotelcard key={room._id} room={room} index={index} />
         ))}
       </div>
-      
     </div>
   );
 };
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ["5173-sudhansuura-hotelbookin-4f0ecwj7o8g.ws-us120.gitpod.io"]
+  }
 });
